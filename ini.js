9656672d2863d2cbdbcb3ed3ef05b449b20cfcc2
agung <agunkootsuki123@gmail.com>
2023-10-28 20:18:58 +0700

    Commit pertama

--- ini.js
+++ ini.js
@@ -9,19 +9,3 @@ window.addEventListener('scroll', function () {
     nav.classList.remove('nav-dark');
   }
 });
-
-let slideIndex = 0;
-showSlide();
-function showSlide() {
-  let i;
-  let slides = document.getElementsByClassName('slide');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlide, 5000);
-}
